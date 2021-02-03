868b98d192c97967ec009c055f531bb8610da467
Ryan Haining <haining.cpp@gmail.com>
2021-02-02 21:04:50 -0800

    Adds example main

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+all: test_main example
+
+example: int_array.o example.o
+	g++ -Wall -Wextra -pedantic -g -std=c++17 example.o int_array.o -o example
+
 test_main: test_array.o test_main.o int_array.o
 	g++ -Wall -Wextra -pedantic -g -std=c++17 test_array.o test_main.o int_array.o -o test_main
+
+example.o: example.cpp
+	g++ -c -Wall -Wextra -pedantic -g -std=c++17 example.cpp
 
 test_array.o: test_array.cpp
 	g++ -c -Wall -Wextra -pedantic -g -std=c++17 test_array.cpp
@@ -11,4 +19,4 @@ int_array.o: int_array.cpp
 	g++ -c -Wall -Wextra -pedantic -g -std=c++17 int_array.cpp
 
 clean:
-	rm -f *.o test_main
+	rm -f *.o test_main example
---
+++ example.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "int_array.h"
+
+// rather than using "static" on a free function for static linkage,
+// in c++, put your top-level but "private" functions in the anonymous
+// namespace. That's just a namespace with no name.
+namespace {
+
+int sum(const dsc::IntArray& array) {
+  int total = 0;
+  for (std::size_t i = 0; i < array.size(); ++i) {
+    total += array[i];
+  }
+  return total;
+}
+
+void print_all(const dsc::IntArray& array) {
+  for (std::size_t i = 0; i < array.size(); ++i) {
+    std::cout << array[i] << '\n';
+  }
+}
+
+} // namespace
+
+int main() {
+  dsc::IntArray array(6);
+  array[0] = 3;
+  array[1] = 6;
+  array[2] = 20;
+  array[3] = 40;
+  array[4] = 100;
+  array[5] = 200;
+
+  print_all(array);
+
+  std::cout << "sum = " << sum(array) << '\n';
+}
